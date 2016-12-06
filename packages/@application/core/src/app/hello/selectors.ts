d4b67718cf6d259258bf5ae7268d13d892bc43a3
Aitor Oses <aitor.oses@gmail.com>
2016-12-06 15:40:34 +0100

    Setting up yarn-autolink

--- packages/@application/core/src/app/hello/selectors.ts
+++ packages/@application/core/src/app/hello/selectors.ts
@@ -1,6 +1,7 @@
 import { NAME } from './constants'
 import { compose } from 'ramda'
-import * as modelHelpers from './model'
+import * as model from './model'
 
 export const getAll = state => state[NAME];
-export const getMessage = compose(modelHelpers.getMessage, getAll)
+
+export const getMessage = compose(model.getMessage, getAll)
